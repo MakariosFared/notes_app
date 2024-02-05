c4730d14457e6aff4fff4d5bd293cb3b27e41103
Makarios <makarios.fared@gmail.com>
2024-02-05 20:57:25 +0200

    edite note done

--- lib/views/widgets/custom_app_bar.dart
+++ lib/views/widgets/custom_app_bar.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:my_note_app/views/widgets/custom_Search_icon.dart';
+import 'package:my_note_app/views/widgets/custom_icon.dart';
 
 class CustomAppBar extends StatelessWidget {
-  const CustomAppBar({super.key, required this.title, required this.icon});
+  const CustomAppBar({super.key, required this.title, required this.icon, this.onPressed});
 
   final String title;
   final IconData icon;
-
+  final void Function()? onPressed;
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -19,6 +19,7 @@ class CustomAppBar extends StatelessWidget {
         ),
         const Spacer(),
         CustomSearchIcon(
+          onPressed: onPressed,
           icon: icon,
         ),
       ],
